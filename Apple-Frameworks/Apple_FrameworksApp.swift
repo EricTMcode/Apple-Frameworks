b6806b1dcf7759d394cef359409d1bddeec38482
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-09-17 14:41:03 +0200

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Eric on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
