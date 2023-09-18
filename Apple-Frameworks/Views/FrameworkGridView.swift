a54d316711c039bff333207a065a0847fbeff25a
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-09-18 15:19:50 +0200

    - Create DetailView

--- Apple-Frameworks/Views/FrameworkGridView.swift
+++ Apple-Frameworks/Views/FrameworkGridView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
+    
     var body: some View {
-        VStack {
-            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                            .onTapGesture {
+                                print("lol")
+                            }
+                    }
+                }
+            }
+            .navigationTitle("🍎 FrameWorks")
         }
     }
 }
@@ -18,24 +32,26 @@ struct FrameworkGridView: View {
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+            .preferredColorScheme(.dark)
     }
 }
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+    
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
 }
